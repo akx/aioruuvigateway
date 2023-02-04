be7c21c42cefebb0838780370fb1094314cb3a33
Aarni Koskela <akx@iki.fi>
2023-02-04 20:20:10 +0200

    Add age information to TagData if available (#4)

--- aioruuvigateway/models.py
+++ aioruuvigateway/models.py
@@ -20,17 +20,28 @@ class TagData(TimestampConversionMixin):
     rssi: int
     timestamp: int
     data: bytes
+    age_seconds: int | None = None
 
     def parse_announcement(self) -> BLEGAPAdvertisement:
         return parse_advertisement_data([self.data])
 
     @classmethod
-    def from_gateway_history_json_tag(cls, mac: str, data: dict) -> TagData:
+    def from_gateway_history_json_tag(
+        cls,
+        mac: str,
+        data: dict,
+        response_timestamp: int | None,
+    ) -> TagData:
+        tag_timestamp = int(data["timestamp"])
+        age_seconds = (
+            (response_timestamp - tag_timestamp) if response_timestamp else None
+        )
         return cls(
             mac=mac,
             rssi=int(data["rssi"]),
-            timestamp=int(data["timestamp"]),
+            timestamp=tag_timestamp,
             data=bytes.fromhex(data["data"]),
+            age_seconds=age_seconds,
         )
 
 
@@ -48,12 +59,17 @@ class HistoryResponse(TimestampConversionMixin):
     @classmethod
     def from_gateway_history_json(cls, data: dict) -> HistoryResponse:
         data = data["data"]
+        response_timestamp = int(data["timestamp"])
         tags = [
-            TagData.from_gateway_history_json_tag(mac=mac, data=tag_data)
+            TagData.from_gateway_history_json_tag(
+                mac=mac,
+                data=tag_data,
+                response_timestamp=response_timestamp,
+            )
             for mac, tag_data in data.get("tags", {}).items()
         ]
         return HistoryResponse(
-            timestamp=int(data["timestamp"]),
+            timestamp=response_timestamp,
             gw_mac=data["gw_mac"],
             coordinates=data.get("coordinates", ""),
             tags=tags,
